556237b0eeeff14d5bdc608fb225498a5ff7893c
Felipe Figueiredo <philsf79@gmail.com>
2016-05-03 15:04:31 -0300

    tipo histológico por sexo

--- scripts/descritivas.R
+++ scripts/descritivas.R
@@ -6,6 +6,12 @@ summary(dados)
 sex_pres.met <- table(Sexo, Presença_Metastase)
 barplot(sex_pres.met, legend = (rownames(sex_pres.met)), beside = T, main = "Presença de Metástase, por sexo", col = c("pink", "blue"))
 
+sex_pres.met <- table(Sexo, Presença_Metastase)
+barplot(sex_pres.met, legend = (rownames(sex_pres.met)), beside = T, main = "Presença de Metástase, por sexo", col = c("pink", "blue"))
+
+sex_hist <- table(Sexo, Tipo_Histologico)
+barplot(sex_hist, legend = (rownames(sex_hist)), beside = T, main = "Presença de Metástase, por sexo", col = c("pink", "blue"))
+
 # 
 
 detach(dados)
