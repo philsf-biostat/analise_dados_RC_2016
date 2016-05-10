641fda82dee2a76ee7269ea76ca8f69a64af7e0e
Felipe Figueiredo <philsf79@gmail.com>
2016-05-10 16:34:07 -0300

    protótipo
    Um exemplo (aprovado) de como as descrições de proporções devem ser tabeladas.

--- scripts/descritivas.R
+++ scripts/descritivas.R
@@ -2,5 +2,8 @@ dados <- read.table("dataset/dataset.dat")
 attach(dados)
 summary(dados)
 
+# Descrição do local do cancer
+round(prop.table(table(Local_Cancer)), digits = 3)*100
+
 detach(dados)
 rm(dados)
