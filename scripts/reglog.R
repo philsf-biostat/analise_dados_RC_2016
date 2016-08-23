bd50ef7916c7f13862b420baa34e6e96a55a1b7d
Felipe Figueiredo <philsf79@gmail.com>
2016-08-23 19:32:06 -0300

    modelos adicionais (print-only)

--- scripts/reglog.R
+++ scripts/reglog.R
@@ -3,6 +3,14 @@ source("scripts/input.R")
 modelo.logistico <- glm(Presenca_Metastase ~ dt, family = binomial)
 summary(modelo.logistico)
 
+summary(glm(Obito ~ dt, family = binomial))
+
 summary(glm(Obito ~ Tipo_Histologico, family = binomial))
+
+summary(glm(Presenca_Metastase ~ Tipo_Histologico, family = binomial))
+
+summary(glm(Presenca_Metastase ~ dt + Tipo_Cirurgia , family = binomial))
+
+summary(glm(Obito ~ dt * Tipo_Histologico, family = binomial))
 
 source("scripts/cleanup.R")
