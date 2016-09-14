0101b58c4c7086d1096626aa79aa67d6f8d3bbe7
Felipe Figueiredo <philsf79@gmail.com>
2016-09-13 21:54:22 -0300

    variável renomeada
    Algumas variáveis foram renomeadas, e os rascunhos antigos não funcionam
    mais.

--- scripts/descritivas.R
+++ scripts/descritivas.R
@@ -1,6 +1,6 @@
 source("scripts/input.R")
 
 # Descrição do local do cancer
-round(prop.table(table(Local_Cancer)), digits = 3)*100
+round(prop.table(table(Localizacao)), digits = 3)*100
 
 source("scripts/cleanup.R")
