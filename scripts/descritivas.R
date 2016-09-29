821b8ba006998d9f81436bec9189e7f04d4bdcd5
Felipe Figueiredo <philsf79@gmail.com>
2016-09-28 23:59:26 -0300

    códigos obsoletos removidos.
    O código que gera a tabela 1 está pronto. Para gerar a tabela final, os
    dados brutos precisam ser corrigidos na planilha.

--- scripts/descritivas.R
+++ scripts/descritivas.R
@@ -1,12 +1,12 @@
 source("scripts/input.R")
 
 # Descrição do local do cancer
-round(prop.table(table(Localizacao)), digits = 3)*100
+# round(prop.table(table(Localizacao)), digits = 3)*100
 
 library(tableone)
 
 tabela1 <- CreateTableOne(data = dados, vars = c("Sexo", "Tipo_Histologico", "Presenca_Metastase", "Local_Recidiva", "Obito", "Causa_Obito", "Localizacao", "Tipo_Cirurgia", "Estadiamento"))
-print(tabela1, showAlllevels = T)
+# print(tabela1, showAlllevels = T)
 summary(tabela1)
 
 source("scripts/cleanup.R")
