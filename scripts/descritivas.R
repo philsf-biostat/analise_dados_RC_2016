422fa1d0d09df6ecea73c5f4df83172c6717b4d7
Felipe Figueiredo <philsf79@gmail.com>
2016-10-04 20:25:23 -0300

    Tabela 1: diversas opções, sem e com estratificações
    - Tabela descritiva pura
    - Tabela estratificada por óbito
    - Estratificada por Metástase
    - Estratificada por Estadiamento

--- scripts/descritivas.R
+++ scripts/descritivas.R
@@ -6,8 +6,22 @@ source("scripts/input.R")
 library(tableone)
 
 tabela1 <- CreateTableOne(data = dados, vars = c("Sexo", "Tipo_Histologico", "Presenca_Metastase", "Local_Recidiva", "Obito", "Localizacao", "Tipo_Cirurgia", "Estadiamento"))
-# print(tabela1, showAlllevels = T)
-summary(tabela1)
+print(tabela1, showAllLevels = T)
+
+# Estratificação: Óbito ####
+t <- CreateTableOne(data = dados, vars = c("Sexo", "Tipo_Histologico", "Presenca_Metastase", "Local_Recidiva", "Localizacao", "Tipo_Cirurgia", "Estadiamento"), strata = "Obito")
+print(t, showAllLevels = T)
+rm(t)
+
+# Estratificação: Metástase ####
+t <- CreateTableOne(data = dados, vars = c("Sexo", "Tipo_Histologico", "Obito", "Local_Recidiva", "Localizacao", "Tipo_Cirurgia", "Estadiamento"), strata = "Presenca_Metastase")
+print(t, showAllLevels = T)
+rm(t)
+
+# Estratificação: Estadiamento ####
+t <- CreateTableOne(data = dados, vars = c("Sexo", "Tipo_Histologico", "Obito", "Presenca_Metastase", "Local_Recidiva", "Localizacao", "Tipo_Cirurgia"), strata = "Estadiamento")
+print(t, showAllLevels = T)
+rm(t)
 
 rm(tabela1)
 source("scripts/cleanup.R")
