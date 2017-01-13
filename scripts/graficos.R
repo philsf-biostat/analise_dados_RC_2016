5d42eafcf5f82c8b9287bc75966ca8080b01b912
Felipe Figueiredo <philsf79@gmail.com>
2017-01-13 07:06:45 -0200

    gráficos atualizados para nova API do mybarplot(). Closes #17

--- scripts/graficos.R
+++ scripts/graficos.R
@@ -15,51 +15,51 @@ mybarplot <- function(tab, desfecho, preditor) {
 }
 
 png("figuras/bp-metastase_sexo.png", 700, 700)
-mybarplot(met.sex, main = "Metástase por Sexo")
+mybarplot(met.sex, desfecho = "Metástase", preditor = "Sexo")
 dev.off()
 
 png("figuras/bp-metastase_hist.png", 700, 700)
-mybarplot(met.hist, main = "Metástase por Tipo histológico")
+mybarplot(met.hist, desfecho = "Metástase", preditor = "Tipo histológico")
 dev.off()
 
 png("figuras/bp-metastase_cirurg.png", 700, 700)
-mybarplot(met.cir, main = "Metástase por Tipo de cirurgia")
+mybarplot(met.cir, desfecho = "Metástase", preditor = "Tipo de cirurgia")
 dev.off()
 
 png("figuras/bp-obitos_hist.png", width = 700, height = 700)
-mybarplot(obt.hist, main = "Óbito por Tipo histológico")
+mybarplot(obt.hist, desfecho = "Óbito", preditor = "Tipo histológico")
 dev.off()
 
 png("figuras/bp-obitos_met.png", 700, 700)
-mybarplot(obt.met, main = "Óbito por presença de metástase")
+mybarplot(obt.met, desfecho = "Óbito", preditor = "Metástase")
 dev.off()
 
 png("figuras/bp-obitos_estad.png", 700, 700)
-mybarplot(obt.est, main = "Óbito por Estadiamento")
+mybarplot(obt.est, desfecho = "Óbito", preditor = "Estadiamento")
 dev.off()
 
 png("figuras/bp-obitos_cirurg.png", width = 700, height = 700)
-mybarplot(obt.cir, main = "Óbito por Tipo de cirurgia")
+mybarplot(obt.cir, desfecho = "Óbito", preditor = "Tipo de cirurgia")
 dev.off()
 
 png("figuras/bp-estad_sexo.png", 700, 700)
-mybarplot(est.sex, main = "Estadiamento por Sexo")
+mybarplot(est.sex, desfecho = "Estadiamento", preditor = "Sexo")
 dev.off()
 
 png("figuras/bp-estad_hist.png", 700, 700)
-mybarplot(est.hist, main = "Estadiamento por Tipo histológico")
+mybarplot(est.hist, desfecho = "Estadiamento", preditor = "Tipo histológico")
 dev.off()
 
 png("figuras/bp-estad_cirurg.png", 700, 700)
-mybarplot(est.cir, main = "Estadiamento por Tipo de cirurgia")
+mybarplot(est.cir, desfecho = "Estadiamento", preditor = "Tipo de cirurgia")
 dev.off()
 
 png("figuras/bp-tempo_cirurg.png", width = 700, height = 700)
-mybarplot(tip.tmp, main = "Tempo de diagnóstico por tipo de cirurgia")
+mybarplot(tip.tmp, desfecho = "Tempo de diagnóstico", preditor = "Tipo de cirurgia")
 dev.off()
 
 rm(met.sex, met.hist, met.obt, met.cir)
 rm(obt.sex, obt.hist, obt.met, obt.cir)
 rm(est.sex, est.hist, est.obt, est.cir)
-rm(tip.tmp)
+# rm(tip.tmp)
 source("scripts/cleanup.R")
