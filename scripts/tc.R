76f52baf7d5370799b0c7678d332b9c29cb016f8
Felipe Figueiredo <philsf79@gmail.com>
2017-01-30 04:29:03 -0200

    não usar attach() nas tabelas de contingência

--- scripts/tc.R
+++ scripts/tc.R
@@ -4,47 +4,47 @@
 ## Metástase ####
 
 ## Mestástase por Genero
-met.sex <- table(Genero, Presenca_Metastase)
+met.sex <- with(dados, table(Genero, Presenca_Metastase))
 
 ## Metástase por tipo histológico
-met.hist <- table(Tipo_Histologico, Presenca_Metastase)
+met.hist <- with(dados, table(Tipo_Histologico, Presenca_Metastase))
 
 ## Metástase por óbito
-met.obt <- table(Obito, Presenca_Metastase)
+met.obt <- with(dados, table(Obito, Presenca_Metastase))
 
 ## Metástase por tipo de cirurgia
-met.cir <- table(Tipo_Cirurgia, Presenca_Metastase)
+met.cir <- with(dados, table(Tipo_Cirurgia, Presenca_Metastase))
 
 ## Óbito ####
 
 # Obito por Genero
-obt.sex <- table(Genero, Obito)
+obt.sex <- with(dados, table(Genero, Obito))
 
 # Obito por tipo histologico
-obt.hist <- table(Tipo_Histologico, Obito)
+obt.hist <- with(dados, table(Tipo_Histologico, Obito))
 
 # Óbito por Metástase
-obt.met <- table(Presenca_Metastase, Obito)
+obt.met <- with(dados, table(Presenca_Metastase, Obito))
 
 ## Metástase por tipo de cirurgia
-obt.cir <- table(Tipo_Cirurgia, Obito)
+obt.cir <- with(dados, table(Tipo_Cirurgia, Obito))
 
 ## Óbito por Estadiamento
-obt.est <- table(Estadiamento, Obito)
+obt.est <- with(dados, table(Estadiamento, Obito))
 
 ## Estadiamento ####
 
 ## Mestástase por Genero
-est.sex <- table(Genero, Estadiamento)
+est.sex <- with(dados, table(Genero, Estadiamento))
 
 ## Estadiamento por tipo histológico
-est.hist <- table(Tipo_Histologico, Estadiamento)
+est.hist <- with(dados, table(Tipo_Histologico, Estadiamento))
 
 ## Estadiamento por óbito
-est.obt <- table(Obito, Estadiamento)
+est.obt <- with(dados, table(Obito, Estadiamento))
 
 ## Estadiamento por tipo de cirurgia
-est.cir <- table(Tipo_Cirurgia, Estadiamento)
+est.cir <- with(dados, table(Tipo_Cirurgia, Estadiamento))
 
 ## Cleanup ####
 # source("scripts/cleanup.R")
